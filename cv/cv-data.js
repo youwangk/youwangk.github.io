0e70b3b3e4b0428baf7f52a197b711e52256cb85
Kim Youwang <youwang.kim@postech.ac.kr>
2026-08-18 15:07:03 +0900

    update review: TOG 2026

--- cv/cv-data.js
+++ cv/cv-data.js
@@ -306,6 +306,7 @@ window.CV_DATA = {
       title: "Reviewer for Journals",
       items: [
         { name: "IEEE Transactions on Pattern Analysis and Machine Intelligence (TPAMI)", years: "2024, 2025" },
+        { name: "ACM Transactions on Graphics (ACM TOG)", years: "2026" },
         { name: "International Journal of Computer Vision (IJCV)", years: "2024, 2025" },
         { name: "IEEE Transactions on Visualization and Computer Graphics (TVCG)", years: "2026" },
         { name: "IEEE Transactions on Multimedia (TMM)", years: "2023" },
